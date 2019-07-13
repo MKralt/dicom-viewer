84f67a4c9c3f49e8cff7953b2d5c2e3d39a3367f
Marcel Kralt <marcel.kralt@gmail.com>
2019-07-13 19:48:28 +0200

    Basic DicomViewer implementation
    DicomViewer will now load any image files selected by the DicomFileUploader, and display
    the first one

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <dicom-file-uploader v-model="dicomFiles"></dicom-file-uploader>
+    <dicom-viewer :files="dicomFiles"></dicom-viewer>
   </div>
 </template>
 
 <script>
 import DicomFileUploader from '@/components/DicomFileUploader.vue'
+import DicomViewer from '@/components/DicomViewer.vue'
 
 export default {
   name: 'app',
   components: {
-    DicomFileUploader
+    DicomFileUploader,
+    DicomViewer
   },
 
   data() {
--- src/components/DicomViewer.vue
+++ src/components/DicomViewer.vue
@@ -1,14 +1,72 @@
 <template>
-    <div></div>
+    <div>
+        <div class="viewer" ref="viewer"></div>
+    </div>
 </template>
 
 <script>
+import dicomParser from 'dicom-parser'
+import cornerstone from 'cornerstone-core'
+import cornerstoneWADOImageLoader from 'cornerstone-wado-image-loader'
+
+cornerstoneWADOImageLoader.external.dicomParser = dicomParser
+cornerstoneWADOImageLoader.external.cornerstone = cornerstone
+
+const config = {
+    webWorkerPath : process.env.BASE_URL + 'cornerstoneWADOImageLoaderWebWorker.js',
+    taskConfiguration: {
+        'decodeTask' : {
+            codecsPath: process.env.BASE_URL + 'cornerstoneWADOImageLoaderCodecs.js'
+        }
+    }
+}
+
+cornerstoneWADOImageLoader.webWorkerManager.initialize(config);
+
 export default {
     props: {
         files: {
             type: Array,
             default: () => []
         }
+    },
+
+    data() {
+        return {
+            cornerstone: cornerstone,
+            imageLoader: cornerstoneWADOImageLoader
+        }
+    },
+
+    computed: {
+        imageIds() {
+            return this.imageLoader ? this.files.map((file) => this.imageLoader.wadouri.fileManager.add(file)) : []
+        }
+    },
+
+    methods: {
+        displayImage(imageId) {
+            this.cornerstone.loadImage(imageId).then((image) => this.cornerstone.displayImage(this.$refs.viewer, image))
+        }
+    },
+
+    watch: {
+        imageIds(newValue) {
+            if(newValue.length > 0) {
+                this.displayImage(newValue[0])
+            }
+        }
+    },
+
+    mounted() {
+        this.cornerstone.enable(this.$refs.viewer)
     }
 }
 </script>
+
+<style lang="scss" scoped>
+.viewer {
+    width: 500px;
+    height: 500px;
+}
+</style>
